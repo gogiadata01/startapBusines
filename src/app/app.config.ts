4a8aaef2bf706c44e5d058200946a2a84cee21ad
lukasordia123 <lksordia@gmail.com>
2024-04-23 02:33:16 +0400

    s

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {AngularFireModule} from "@angular/fire/";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import {AngularFirestoreModule} from ""
+
+
 
 import { routes } from './app.routes';
+import {firebaseConfig} from "./core/constans/constants";
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes),
+    importProvidersFrom([AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule,
+      AngularFireDatabaseModule,
+      AngularFirestoreModule
+    ]) ]
 };
