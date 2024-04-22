4b434c20821520d302a3614e1348be3aeb079a71
lukasordia123 <lksordia@gmail.com>
2024-04-22 22:19:53 +0400

    firebase update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"", 
+        component:RegisterComponent
+    },
+    {
+        path:"LogIn",
+        component:LogInComponent
+    }
+];
