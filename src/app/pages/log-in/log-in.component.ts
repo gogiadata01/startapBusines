1a727c2641376ebdca2acf35d2280b89ce0ae73b
gogiadata01 <gogia.data11@gmail.com>
2024-04-30 19:53:20 +0400

    up04

--- src/app/pages/log-in/log-in.component.ts
+++ src/app/pages/log-in/log-in.component.ts
@@ -1,12 +1,15 @@
 import { NgIf } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators, } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-log-in',
   standalone: true,
   imports: [ReactiveFormsModule,
-  NgIf],
+  NgIf, RouterLink],
   templateUrl: './log-in.component.html',
   styleUrl: './log-in.component.scss'
 })
