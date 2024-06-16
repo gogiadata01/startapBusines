ccc38270daa6846c080aefc5ce8182447251a4ea
lukasordia123 <lksordia@gmail.com>
2024-06-16 22:26:46 +0400

    up

---
+++ src/app/pages/uni-programs/uni-programs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uni-programs',
+  standalone: true,
+  imports: [],
+  templateUrl: './uni-programs.component.html',
+  styleUrl: './uni-programs.component.scss'
+})
+export class UniProgramsComponent {
+
+}
