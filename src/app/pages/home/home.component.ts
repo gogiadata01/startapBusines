dac73ba8c91f0c27d696e6c169251121e8234aed
gogiadata01 <gogia.data11@gmail.com>
2024-09-09 14:16:44 +0400

    Home page update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  leaders1 = ['Person 1', 'Person 2', 'Person 3', 'Person 4'];
+  leaders2 = ['Person 5', 'Person 6', 'Person 7', 'Person 8'];
+  leaders3 = ['Person 9', 'Person 10', 'Person 11', 'Person 12'];
 }
