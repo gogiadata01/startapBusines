59449ecc52208ee7393632a4d3e47246b6900579
lukasordia123 <lksordia@gmail.com>
2024-09-09 14:56:53 +0400

    ..

---
+++ src/app/pages/quize/quize.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quize',
+  standalone: true,
+  imports: [],
+  templateUrl: './quize.component.html',
+  styleUrl: './quize.component.scss'
+})
+export class QuizeComponent {
+
+}
