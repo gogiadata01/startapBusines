dd13ef94677d8bb43588ad8714e5b4faeac4d6cf
lukasordia123 <lksordia@gmail.com>
2025-03-14 19:05:33 +0400

    ს

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,7 @@ import {HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import 'zone.js';  // Included with Angular CLI.
 
 bootstrapApplication(AppComponent, {
   providers: [
